86176313651e292afbd5a73657d5f5ae87a94d0d
aaliaaa <aaliaaa@163.com>
2024-06-07 19:27:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
 recent_days = 7
-ipv_type = "ipv6"
+ipv_type = "all"
 domain_blacklist = ["epg.pw"]
 url_keywords_blacklist = []
 open_subscribe = True
@@ -29,6 +29,11 @@ subscribe_urls = [
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://raw.githubusercontent.com/Guovin/TV/gd/result.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt",
+    "https://fanmingming.com/txt?url=https://live.fanmingming.com/tv/m3u/ipv6.m3u",
    ]
 open_multicast = True
 region_list = ["all"]
