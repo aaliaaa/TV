c588900ceca9e1ce41190698893a9c54ae19374b
aaliaaa <aaliaaa@163.com>
2024-06-08 10:53:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "广东珠江",
     "CCTV-1",
     "CCTV-5",
-    "CCTV-5+",
-    "CCTV-13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
-    "翡翠台",
-]
+    ]
 open_online_search = False
 favorite_page_num = 5
 default_page_num = 5
